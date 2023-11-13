61b6f23418f01ec316d5ce7f8b11bc5c24f47729
divyanshsingh101 <150357868+divyanshsingh101@users.noreply.github.com>
2023-11-13 14:18:48 +0530

    div

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-gsap.from("#page1 h1",{
+gsap.from("#text1 h1",{
     y:100,
     opacity:0,
     delay:0.5,
     duration:0.8,
     stagger:0.4
 })
-   
+function scrollToNextPage() {
+    var currentScroll = window.scrollY;
+    var pageHeight = window.innerHeight;
+    var nextPageScroll = Math.ceil((currentScroll + 1) / pageHeight) * pageHeight;
+    window.scrollTo({
+      top: nextPageScroll,
+      behavior: 'smooth'
+    });
+  }
+
+
+
+
+
+
