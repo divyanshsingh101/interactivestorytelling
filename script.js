4a3398038f4badd7477d33ed89ca7268b0f9f6b5
divyanshsingh101 <150357868+divyanshsingh101@users.noreply.github.com>
2023-11-24 23:14:48 +0530

    final1

--- script.js
+++ script.js
@@ -14,10 +14,13 @@ function scrollToNextPage() {
       behavior: 'smooth'
     });
   }
+function scrollTwoPages() {
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    window.scrollBy(0, viewportHeight * 2);
+  }
  
-  function animateElement() {
+function animateElement() {
     gsap.from("#ready", {
-    
     opacity:0,
     delay:2,
     duration:2,
@@ -28,7 +31,36 @@ function scrollToNextPage() {
   // Add a click event listener to the button
   document.getElementById("buttonToThirdPage").addEventListener("click", animateElement);
  
+  
+  function displayText1() {
+    var textElement = document.getElementById('c21');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText2() {
+    var textElement = document.getElementById('c31');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText3() {
+    var textElement = document.getElementById('c32');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText4() {
+    var textElement = document.getElementById('c41');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText5() {
+    var textElement = document.getElementById('c42');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText6() {
+    var textElement = document.getElementById('c51');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
+  function displayText7() {
+    var textElement = document.getElementById('c52');
+    textElement.style.visibility = (textElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+  }
 
- 
 
 
+  
